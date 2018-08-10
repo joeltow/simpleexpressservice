5130ceeaabdb88f49977d9c82eb4b24969417a0b
Unknown <joel.tow@gmail.com>
2018-08-10 12:23:05 +1000

    Add timestamps to log messages

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss.l' });
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Sweet!'));
+app.get('/', (req, res) => {
+    console.log('Received request');
+    res.send('Sweet!');
+});
 
 app.listen(3000, () => console.log('listending on port 3000'));
